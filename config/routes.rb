29089cf2cfb47bf2ae6371c9ca7f15fbb000dfe3
Laura Brooks <laura.brooks0@outlook.com>
2022-01-10 16:11:57 +0900

    added edit and delete icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'magic_items#index'
   resources :magic_items
   get '/search', to: "magic_items#search"
 end
